54d9bed5c86a0e8d43f98730a63f951c37d0916b
Johnathan Cornella <jcornella11@hotmail.com>
2021-07-25 19:09:26 -0700

    Add project files.

---
+++ GUI Minesweeper/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        static public board myBoard = new board(10);
+        public Button[,] btnGrid = new Button[myBoard.size, myBoard.size];
+        bool endgame = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateButtonGrid();
+        }
+
+        public void populateButtonGrid() 
+        {
+            //function to fill the panel control with buttons
+            int buttonSize = panel1.Width / myBoard.size; //Calculate the width of each grid button
+            panel1.Height = panel1.Width;
+
+            //Nested Loop to Create buttons and place them in the panel
+            for (int x = 0; x < myBoard.size; x++) 
+            {
+                for (int y = 0; y < myBoard.size; y++) 
+                {
+                    //Create the Button
+                    btnGrid[x, y] = new Button();
+
+                    //Make each button square
+                    btnGrid[x, y].Width = buttonSize;
+                    btnGrid[x, y].Height = buttonSize;
+
+                    btnGrid[x, y].Click += Grid_Button_Click; //Click event for each button
+                    panel1.Controls.Add(btnGrid[x, y]); //Place Button on the Panel
+                    btnGrid[x, y].Location = new Point(buttonSize * x, buttonSize * y); //Position it with x, y cordinates
+
+                    btnGrid[x, y].Text = x.ToString() + "|" + y.ToString();
+
+                    //Tag attribute will hold row and column number in a string
+                    btnGrid[x, y].Tag = x.ToString() + "|" + y.ToString();
+                }
+            }
+        }
+
+        private void Grid_Button_Click(object? sender, EventArgs e)
+        {
+            //Get the Row and Column Number of the Button that was just Clicked
+            string[] strArr = (sender as Button).Tag.ToString().Split("|");
+            int x = int.Parse(strArr[0]);
+            int y = int.Parse(strArr[1]);
+
+            //Display the Live Neighbors for the clicked square
+            btnGrid[x, y].Text = myBoard.grid[x, y].liveNeighbors.ToString();
+
+            //Set the Visited Property to true on the Selected Square
+            myBoard.grid[x, y].visited = true;
+
+            //Call recursive flood fill on the clicked square
+            myBoard.floodFill(x, y);
+
+            //Loop through all of the Squares on the Grid
+            //If the square has been visited reveal the contents
+
+            for (int j = 0; j < myBoard.size; j++)
+            {
+                for (int k = 0; k < myBoard.size; k++) 
+                {
+                    if (myBoard.grid[j, k].visited == true) 
+                    {
+                        btnGrid[j, k].Text = myBoard.grid[j, k].liveNeighbors.ToString();
+                    }
+                }
+            }
+
+            //reset the background color of all buttons to the default (original) color. 
+            for (int i = 0; i < myBoard.size; i++)
+            {
+                for (int j = 0; j < myBoard.size; j++)
+                {
+                    btnGrid[i, j].BackColor = default(Color);
+                }
+            }
+
+            //set the background of the clicked button to something different
+            (sender as Button).BackColor = Color.Cornsilk;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GUI Minesweeper/board.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Minesweeper
+{
+    public class board
+    {
+        public int size { get; set; } //Square so Length & Width are Same Dimensions
+        public Cell[,] grid; //2D Array of Cell Objects
+        public double Difficulty = 10; //Set Dificulty to 10% mines
+
+        public board(int Size)
+        {
+            size = Size;
+            grid = new Cell[Size, Size];
+            for (int j = 0; j < grid.GetLength(0); j++)
+            {
+                for (int k = 0; k < grid.GetLength(1); k++)
+                {
+                    grid[j, k] = new Cell();
+                    grid[j, k].row = j;
+                    grid[j, k].column = k;
+                }
+            }
+            setupLiveNeighbors();
+            calculateLiveNeighbords();
+        }
+
+        public void setupLiveNeighbors()
+        {
+
+            for (int j = 0; j < grid.GetLength(0); j++)
+            {
+
+                for (int k = 0; k < grid.GetLength(1); k++)
+                {
+                    Random random = new Random();
+                    int percent = random.Next(0, 100);
+
+                    if (percent <= Difficulty)
+                    {
+                        grid[j, k].Live = true;
+                        grid[j, k].liveNeighbors = 9;
+                    }
+                    else
+                    {
+                        grid[j, k].Live = false;
+                    }
+                }
+            }
+        }
+
+        public void calculateLiveNeighbords()
+        {
+            for (int j = 0; j < grid.GetLength(0); j++)
+            {
+                for (int k = 0; k < grid.GetLength(1); k++)
+                {
+                    int liveCount = 0;
+
+                    if (grid[j, k].Live == true)
+                    {
+                        liveCount = 9;
+                        continue;
+                    }
+
+                    if (j > 0 && k > 0)
+                    {
+                        if (grid[j - 1, k - 1].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    if (k > 0)
+                    {
+                        if (grid[j, k - 1].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+
+                    }
+
+                    if (j < grid.GetLength(0) - 1 && k > 0)
+                    {
+                        if (grid[j + 1, k - 1].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    if (j > 0)
+                    {
+                        if (grid[j - 1, k].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    if (j < grid.GetLength(0) - 1)
+                    {
+                        if (grid[j + 1, k].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    if (j > 0 && k < grid.GetLength(1) - 1)
+                    {
+                        if (grid[j - 1, k + 1].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    if (k < grid.GetLength(1) - 1)
+                    {
+                        if (grid[j, k + 1].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    if (j < grid.GetLength(0) - 1 && k < grid.GetLength(1) - 1)
+                    {
+                        if (grid[j + 1, k + 1].Live == true)
+                        {
+                            liveCount = liveCount + 1;
+                        }
+                    }
+
+                    grid[j, k].liveNeighbors = liveCount;
+                }
+            }
+        }
+
+        public void floodFill(int j, int k)
+        {
+            //8 Direction Flood Fill
+            grid[j, k].visited = true;
+
+            //South
+            if (k < grid.GetLength(1) - 1)
+            {
+                if (grid[j, k + 1].liveNeighbors == 0 && grid[j, k + 1].visited == false)
+                {
+                    floodFill(j, k + 1);
+                }
+            }
+
+            //West
+            if (j > 0)
+            {
+                if (grid[j - 1, k].liveNeighbors == 0 && grid[j - 1, k].visited == false)
+                {
+                    floodFill(j - 1, k);
+                }
+            }
+
+            //North
+            if (k > 1)
+            {
+                if (grid[j, k - 1].liveNeighbors == 0 && grid[j, k - 1].visited == false)
+                {
+                    floodFill(j, k - 1);
+                }
+            }
+
+            //East
+            if (j < grid.GetLength(0) - 1)
+            {
+                if (grid[j + 1, k].liveNeighbors == 0 && grid[j + 1, k].visited == false)
+                {
+                    floodFill(j + 1, k);
+                }
+            }
+
+            //South East
+            if (j < grid.GetLength(0) - 1 && k < grid.GetLength(1) - 1)
+            {
+
+                if (grid[j + 1, k + 1].liveNeighbors == 0 && grid[j + 1, k + 1].visited == false)
+                {
+                    floodFill(j + 1, k + 1);
+                }
+            }
+
+            //South West
+            if (j > 0 && k < grid.GetLength(1) - 1)
+            {
+                if (grid[j - 1, k + 1].liveNeighbors == 0 && grid[j - 1, k + 1].visited == false)
+                {
+                    floodFill(j - 1, k + 1);
+                }
+            }
+
+            //North East
+            if (j < grid.GetLength(0) - 1 && k > 0)
+            {
+                if (grid[j + 1, k - 1].liveNeighbors == 0 && grid[j + 1, k - 1].visited == false)
+                {
+                    floodFill(j + 1, k - 1);
+                }
+            }
+
+            //North West
+            if (j > 0 && k > 0)
+            {
+                if (grid[j - 1, k - 1].liveNeighbors == 0 && grid[j - 1, k - 1].visited == false)
+                {
+                    floodFill(j - 1, k - 1);
+                }
+            }
+
+        }
+    }
+}
+
